3abdc2254b4b7bf92528b5ec823a618d25752ee2
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-01-14 17:41:14 +0530

    Update file 1/14/2025, 5:41:06 PM

--- 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.cpp
+++ 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.cpp
@@ -1,19 +1,6 @@
-class Solution {
-public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        unordered_set<int> set;
-
-        vector<int> ans(A.size(), 0);
-        for(int i = 0; i<A.size(); i++){
-            set.insert(A[i]);
-
-            for(int j = 0; j<=i; j++){
-                if(set.find(B[j]) != set.end()){
-                    ans[i]++;
-                }
-            }
-        }
-
-        return ans;
-    }
-};
+unordered_set<int> s; vector<int> ans;
+for (int i = 0; i < A.size(); i++) {
+    s.insert(A[i]); s.insert(B[i]);
+    ans.push_back(2 * (i + 1) - s.size());
+}
+return ans;
