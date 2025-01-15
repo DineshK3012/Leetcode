e4fc10857fd8b9e266ff5e147276c2334dba051e
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-01-15 19:42:56 +0530

    Update file 1/15/2025, 7:42:48 PM

--- 331-VerifyPreorderSerializationofaBinaryTree/331-VerifyPreorderSerializationofaBinaryTree.java
+++ 331-VerifyPreorderSerializationofaBinaryTree/331-VerifyPreorderSerializationofaBinaryTree.java
@@ -1,33 +1,20 @@
 class Solution {
-    public boolean checkSubtree(String[] nodes, int[] counter){
-        if(counter[0] >= nodes.length){
-            return false;
-        }
-
-        if(nodes[counter[0]].equals("#")){
-            return true;
-        }
-
-        // System.out.println(nodes[counter[0]] + " " + counter[0]);
-
-        counter[0]++;
-        boolean left = checkSubtree(nodes, counter);
-        if(!left){
-            return false;
-        }
-
-        counter[0]++;
-        boolean right = checkSubtree(nodes, counter);
-       
-        return right;
-    }
-
     public boolean isValidSerialization(String preorder) {
         String[] nodes = preorder.split(",");
 
-        int[] counter = new int[1];
-        boolean res = checkSubtree(nodes, counter);
-        System.out.println(res + " " + counter[0]);
-        return res && (counter[0] == nodes.length-1);
+        int vacancy = 1;
+        for(String s: nodes){
+            if(vacancy == 0){
+                return false;
+            }
+
+            if(s.equals("#")){
+                vacancy--;
+            }else{
+                vacancy++;
+            }
+        }
+
+        return vacancy == 0;
     }
 }
