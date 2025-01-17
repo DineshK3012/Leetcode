b812b7fbb4ffa38d92940872970b9ffeee77cff5
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-01-17 13:29:52 +0530

    Update file 1/17/2025, 1:29:43 PM

--- 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
+++ 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
@@ -1,22 +1,10 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int n = derived.length;
-        int f = 0;
-        int p = 0;
-        for(int i = 1; i<n; i++){
-            p =  p ^ derived[i-1];
+       int xor = 0;
+        for(int x: derived){
+            xor ^= x;
         }
 
-        if((p^derived[n-1]) == f){
-            return true;
-        }
-
-        f = 1;
-        p = 1;
-        for(int i = 1; i<n; i++){
-            p =  p ^ derived[i-1];
-        }
-
-        return (p^derived[n-1]) == f;
+        return xor == 0;
     }
 }
