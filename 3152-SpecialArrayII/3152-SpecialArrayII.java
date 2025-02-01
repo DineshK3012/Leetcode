5c2022f7df5c9f8d9ce0400fc9afc2f9e51cb402
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-02-01 11:06:21 +0530

    Update file 2/1/2025, 11:06:08 AM

--- 3152-SpecialArrayII/3152-SpecialArrayII.java
+++ 3152-SpecialArrayII/3152-SpecialArrayII.java
@@ -2,19 +2,20 @@ class Solution {
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
         boolean[] ans = new boolean[queries.length];
         int n = nums.length;
-        int[] ps = new int[n];
+        int[] mr = new int[n];
 
-        for(int i = 1; i<n; i++){
-            if(nums[i] % 2 == nums[i-1] % 2){
-                ps[i] = ps[i-1]+1;
+        mr[n-1] = n-1;
+        for(int i = n-2; i>=0; i--){
+            if(nums[i] % 2 != nums[i+1] % 2){
+                mr[i] = mr[i+1];
             }else{
-                ps[i] = ps[i-1];
+                mr[i] = i;
             }
         }
 
         for(int i = 0; i<queries.length; i++){
             int[] q = queries[i];
-            ans[i] = (ps[q[1]] - ps[q[0]]) == 0;
+            ans[i] = q[1] <= mr[q[0]];
         }
 
         return ans;
