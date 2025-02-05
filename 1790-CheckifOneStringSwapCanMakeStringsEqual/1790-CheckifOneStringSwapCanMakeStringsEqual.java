ff8efe74b2121b4871adcd46d778ae00e7e5ebf2
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-02-05 11:06:04 +0530

    Create file

---
+++ 1790-CheckifOneStringSwapCanMakeStringsEqual/1790-CheckifOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int freq[] = new int[26];
+        int count = 0;
+        for(int i = 0; i<s1.length(); i++){
+            freq[s1.charAt(i)-'a']++;
+            freq[s2.charAt(i)-'a']--;
+
+            if(s1.charAt(i) != s2.charAt(i)){
+                count++;
+            }
+        }
+
+        for(int f: freq){
+            if(f != 0){
+                return false;
+            }
+        }
+
+        return count <= 2;
+    }
+}
