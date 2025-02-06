2f6585747713ae4652c66be10c2d19fe52ae2672
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-02-06 09:27:44 +0530

    Create file

---
+++ 1726-TuplewithSameProduct/1726-TuplewithSameProduct.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        Arrays.sort(nums);
+
+        HashMap<Long, Integer> map = new HashMap<>();
+        int n = nums.length;
+        int count = 0;
+        for(int i = 0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){
+                long p = nums[i] * nums[j];
+
+                if(map.containsKey(p)){
+                    count += (map.get(p) * 8);
+                }
+
+                map.put(p, map.getOrDefault(p, 0) + 1);
+            }
+        }
+
+        return count;
+    }
+}
