3663a99fef9708c7fd2ff97fff294479da19af6a
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-02-09 11:33:02 +0530

    File updated at 2/9/2025, 11:32:47 AM

--- 2364-CountNumberofBadPairs/2364-CountNumberofBadPairs.cpp
+++ 2364-CountNumberofBadPairs/2364-CountNumberofBadPairs.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    long long countBadPairs(vector<int>& nums) {
-        long long ans = 0;
-        unordered_map<int, int> map;
-
-        long long n = nums.size();
-        for(int i = 0; i<n; i++){
-            int diff = nums[i] - i;
-
-            if(map.find(diff) != map.end()){
-                ans += map[diff];
-            }
-
-            map[diff]++;
+    long long countBadPairs(vector<int>& nums){
+        long long int b=nums.size(),g=0,k=0;
+        for(int i=0;i<b;i++){
+            nums[i]=i-nums[i];
         }
-
-        long long totalPairs = (n * (n-1))/2;
-
-        return (totalPairs - ans);
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<b-1;i++){
+            if(nums[i]==nums[i+1]){k++;}
+            else{g=g+(k*(k+1))/2;k=0;}
+        }
+        return (b*(b-1))/2-g-(k*(k+1))/2;
     }
 };
