53e4c38e206c6c6836a335aae26ee07876e3b4aa
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-02-13 09:55:33 +0530

    File updated at 2/13/2025, 9:55:16 AM

--- 653-TwoSumIV-InputisaBST/653-TwoSumIV-InputisaBST.cpp
+++ 653-TwoSumIV-InputisaBST/653-TwoSumIV-InputisaBST.cpp
@@ -9,25 +9,68 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    bool helper(TreeNode * root, int target, unordered_set<int> & set){
-        if(root == NULL)    return false;
-        
-        bool left = helper(root->left, target, set);
-        if(left)    return true;
-        
-        if(set.find(target-root->val) != set.end()){
-            return true;
-        }
-        set.insert(root->val);
-        
-        return helper(root->right, target, set);
+class BSTIterator{
+private: 
+    stack<TreeNode *> stk;
+    bool reverse;
+
+public: 
+    BSTIterator(TreeNode* root, bool isReverse){
+        reverse = isReverse;
+        pushToStack(root);
     }
 
+    void pushToStack(TreeNode* node){
+        if(node == NULL)    return;
+
+        stk.push(node);
+
+        if(reverse){
+            while(node->right != NULL){
+                stk.push(node->right);
+                node = node->right;
+            }
+        }else{
+            while(node->left != NULL){
+                stk.push(node->left);
+                node = node->left;
+            }
+        }
+    }
+    
+    int next(){
+        TreeNode* nxt = stk.top();
+        stk.pop();
+
+        if(reverse){
+            pushToStack(nxt->left);
+        }else{
+            pushToStack(nxt->right);
+        }
+
+        return nxt->val;
+    }
+};
+
+class Solution {
+public:
     bool findTarget(TreeNode* root, int k) {
-        unordered_set<int> set;
-        
-        return helper(root, k, set);
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int i = l.next();
+        int j = r.next();
+        while(i != j){
+            // cout<<i<<" "<<j<<endl;
+            if((i + j) == k){
+                return true;
+            }else if((i + j) > k){
+                j = r.next();
+            }else{
+                i = l.next();
+            }
+        }
+
+        return false;
     }
 };
