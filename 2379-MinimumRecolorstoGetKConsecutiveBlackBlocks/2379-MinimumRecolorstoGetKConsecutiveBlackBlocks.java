4c7cd8e54c1fc38ed7d7ba0185aa34e61fa30413
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-03-08 06:30:34 +0530

    File updated at 3/8/2025, 6:30:12 AM

--- 2379-MinimumRecolorstoGetKConsecutiveBlackBlocks/2379-MinimumRecolorstoGetKConsecutiveBlackBlocks.java
+++ 2379-MinimumRecolorstoGetKConsecutiveBlackBlocks/2379-MinimumRecolorstoGetKConsecutiveBlackBlocks.java
@@ -2,21 +2,12 @@ class Solution {
     public int minimumRecolors(String blocks, int k) {
         int white = 0;
         for(int i = 0; i<k; i++){
-            if(blocks.charAt(i) == 'W'){
-                white++;
-            }
+            white += (blocks.charAt(i) == 'W'? 1: 0);
         }
 
         int ans = white;
         for(int i = k; i<blocks.length(); i++){
-            if(blocks.charAt(i) == 'W'){
-                white++;
-            }
-
-            if(blocks.charAt(i-k) == 'W'){
-                white--;
-            }
-
+            white += (blocks.charAt(i) == 'W'? 1: 0) - (blocks.charAt(i-k) == 'W' ? 1: 0);
             ans = Math.min(ans, white);
         }
 
