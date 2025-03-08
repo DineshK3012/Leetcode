6bd6b80378e8b0d057ec27a569ecb22cba1b3ad4
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-03-08 12:29:58 +0530

    File updated at 3/8/2025, 12:29:37 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,23 +1,37 @@
 class Solution {
-    private int helper(int i, int nums[], int[] dp){
-        if(i >= nums.length)    return 0;
+    // private int helper(int i, int nums[], int[] dp){
+    //     if(i >= nums.length)    return 0;
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        //not robbing the current house
-        int ans1 = helper(i+1, nums, dp);
+    //     //not robbing the current house
+    //     int ans1 = helper(i+1, nums, dp);
 
-        //robbing the current house
-        int ans2 = nums[i] + helper(i+2, nums, dp);
+    //     //robbing the current house
+    //     int ans2 = nums[i] + helper(i+2, nums, dp);
 
-        return dp[i] = Math.max(ans1, ans2);
-    }
+    //     return dp[i] = Math.max(ans1, ans2);
+    // }
 
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        if(n == 1)  return nums[0];
+        if(n == 2)  return Math.max(nums[0], nums[1]);
 
-        return helper(0, nums, dp);
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i<n; i++){
+            //not robbing the current house
+            int ans1 = dp[i-1];
+
+            //robbing the current house
+            int ans2 = nums[i] + dp[i-2];
+
+            dp[i] = Math.max(ans1, ans2);
+        }
+
+        return dp[n-1];
     }
 }
