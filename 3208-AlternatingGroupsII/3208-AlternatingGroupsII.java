45a727e224ef25c8cf252bc3ad0f3de84816fa9d
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-03-09 09:55:48 +0530

    File updated at 3/9/2025, 9:55:25 AM

--- 3208-AlternatingGroupsII/3208-AlternatingGroupsII.java
+++ 3208-AlternatingGroupsII/3208-AlternatingGroupsII.java
@@ -1,10 +1,10 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
         int n = colors.length;
-        int[] temp = new int[2*n];
+        int[] temp = new int[n+k];
 
         temp[0] = 1;
-        for(int i = 1; i<2*n; i++){
+        for(int i = 1; i<n+k-1; i++){
             if(colors[i % n] != colors[(i-1)%n]){
                 temp[i] = temp[i-1] + 1;
             }else{
@@ -14,7 +14,7 @@ class Solution {
 
         int ans = temp[k-1] == k? 1: 0;
         for(int i = k; i<n + k - 1; i++){
-            if(temp[i] == k || temp[i] - temp[i-k] == k){
+            if(temp[i] >= k){
                 ans++;
             }
         }
