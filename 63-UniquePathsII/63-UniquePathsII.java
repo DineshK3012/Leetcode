95ae57fcb1e173fd087f5fa77613ee2e090b9576
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-03-11 00:23:18 +0530

    File updated at 3/11/2025, 12:22:55 AM

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -5,34 +5,27 @@ class Solution {
         }
 
         int m = grid.length, n = grid[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = 1;
-        for(int i = 1; i<m; i++){
-            if(grid[i][0] == 1){
-                dp[i][0] = 0;
-            }else{
-                dp[i][0] = dp[i-1][0];
-            }
-        }
-
+        int[] dp = new int[n];
+        dp[0] = 1;
         for(int i = 1; i<n; i++){
              if(grid[0][i] == 1){
-                dp[0][i] = 0;
+                dp[i] = 0;
             }else{
-                dp[0][i] = dp[0][i-1];
+                dp[i] = dp[i-1];
             }
         }
 
         for(int i = 1; i<m; i++){
+            dp[0] = grid[i][0] == 1? 0: dp[0];
             for(int j = 1; j<n; j++){
                 if(grid[i][j] == 1){
-                    dp[i][j] = 0;
+                    dp[j] = 0;
                 }else{
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    dp[j] = dp[j] + dp[j-1];
                 }
             }
         }
 
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 }
