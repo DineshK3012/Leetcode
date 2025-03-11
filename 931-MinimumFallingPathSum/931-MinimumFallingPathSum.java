f7e2178a8f94e4bd7c5e3979481e3cd7bc740b0f
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2025-03-11 10:10:56 +0530

    File updated at 3/11/2025, 10:10:34 AM

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
@@ -1,28 +1,33 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int[][] dp = new int[n][n];
+        int[] dp = new int[n];
+        int[] curr = new int[n];
 
         //base case
         for(int i = 0; i<n; i++){
-            dp[0][i] = matrix[0][i];
+            dp[i] = matrix[0][i];
         }
 
         for(int i = 1; i<n; i++){
+            curr = new int[n];
+            Arrays.fill(curr, Integer.MAX_VALUE);
+
             for(int j = 0; j<n; j++){
                 int l = Math.max(0, j-1);
                 int r = Math.min(n-1, j+1);
 
-                dp[i][j] = Integer.MAX_VALUE;
                 for(int k = l; k <= r; k++){
-                    dp[i][j] = Math.min(dp[i][j], matrix[i][j] + dp[i-1][k]);                    
+                    curr[j] = Math.min(curr[j], matrix[i][j] + dp[k]);                    
                 }
             }
+
+            dp = curr;
         }   
 
         int ans = Integer.MAX_VALUE;
         for(int i = 0; i<n; i++){
-            ans = Math.min(ans, dp[n-1][i]);
+            ans = Math.min(ans, dp[i]);
         }
 
         return ans;
